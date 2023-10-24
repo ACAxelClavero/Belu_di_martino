1faaab8a7188ed42cd7259f0dd596fd3b2561314
Pini Clavero <maxi.a.clavero@gmail.com>
2023-10-24 17:30:12 -0300

    agregado de descripcion modeling

--- src/pages/modeling.jsx
+++ src/pages/modeling.jsx
@@ -12,7 +12,11 @@ function Modeling() {
         <h1 className='titular scale-up-horizontal-center' >MODELING                        </h1>
       </div>
       <div className='flex_contenedor'>
-        <p className='parrafo'>Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las</p>
+        <p className='parrafo'>In the world of modeling, I uncover a unique passion for artistic expression. Each session is a canvas where I blend fashion and authenticity, creating captivating visual stories.
+<br></br><br></br>
+I explore diverse styles, from high fashion to contemporary elegance, aiming to convey emotions through each image. Modeling allows me to challenge boundaries and transform reality into art.
+<br></br><br></br>
+Discover my portfolio for a journey of self-discovery and artistic beauty, where every photo tells a story.</p>
         <iframe className='dance_imagen' src="https://www.youtube.com/embed/VWGK3wO33Gw?si=kWYORPiX2MXZZWan" title="Modeling video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
       </div>
       <h2 className='titular_galeria'>GALLERY</h2>
